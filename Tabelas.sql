04186811dc56b5cc05d6446bf643021f5d43f94e
Janaina Garbugio <contato.jgarbugio@gmail.com>
2021-07-23 02:15:33 -0300

    Repositório particular | Projeto Integrador

---
+++ Tabelas.sql
@@ -0,0 +1,31 @@
+create database db_projetoint;
+
+use db_projetoint;
+
+CREATE TABLE tb_postagem (
+    Id INT NOT NULL  PRIMARY KEY AUTO_INCREMENT,
+    Título VARCHAR(255) NOT NULL,
+    Link VARCHAR(255) NOT NULL,
+    Plataforma_de_retirada VARCHAR(30) NOT NULL,
+    Autor VARCHAR(30) NOT NULL,
+	Data_mensagem DATETIME
+);
+
+CREATE TABLE tb_usuario (
+    Id INT NOT NULL  PRIMARY KEY AUTO_INCREMENT,
+    Nome_completo VARCHAR(255) NOT NULL,
+    Data_de_Nascimento date NOT NULL,
+    Telefone VARCHAR(30) NOT NULL,
+    Senha VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE tb_tema (
+    Id INT NOT NULL  PRIMARY KEY AUTO_INCREMENT,
+    Autor VARCHAR(255) NOT NULL,
+    Título VARCHAR(255) NOT NULL,
+    Texto VARCHAR(255) NOT NULL,
+    Link VARCHAR(255),
+    Id_usuário INT
+);
+
+alter table tb_tema add foreign key (Id_usuário) references tb_usuário(Id);
